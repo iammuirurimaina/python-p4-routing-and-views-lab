3b2beef005477806310a230d197f901501687f93
maina <ian.maina@student.moringaschool.com>
2023-09-18 14:11:45 +0300

    12 passing test with only 1 test failing

--- server/app.py
+++ server/app.py
@@ -1,8 +1,53 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, abort
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_string(parameter):
+   
+    print (parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    num = ""
+    for i in range(parameter):
+        num += '/n' + str(i)
+    return '<p>n + num</p>'
+    
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            abort(400, description="Division by zero is not allowed.")
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        abort(400, description="Invalid operation.")
+    
+    return str(result)
+    
+
+
+    
+
+ 
+        
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
